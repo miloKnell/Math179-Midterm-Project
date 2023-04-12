207509a6b2ac019db100eadab6b1e9fca639efce
David Chen <davidc3287@gmail.com>
2023-04-12 16:09:24 -0700

    clean up ord

--- Code/svm.py
+++ Code/svm.py
@@ -14,6 +14,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from mord import OrdinalRidge
 from pathlib import Path
+from OrdClass import OrdClass
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -146,72 +147,14 @@ y_pred_ord = ord_reg.predict(X_test)
 print('Mord OrdinalRidge classification report:\n', classification_report(y_test, y_pred_ord))
 
 # %%
-from sklearn.base import BaseEstimator
-class OrdClass(BaseEstimator):
-  """
-  Helper class that solves ordinal classification (classes that have an order to them eg cold,warm,hot)
-  """
-  def __init__(self,classifier=None,clf_args=None):
-    """
-    y needs to be a number that start from 0 and increments by 1
-    classifier object needs to be able to return a probability
-    """
-    self.classifier = classifier
-    self.clfs = []
-    self.clf_args = clf_args
-    self.final_prob = None
-  
-  def fit(self,X,y,**fit):
-    self.X = X
-    self.y = y
-    import copy
-    no_of_classifiers = np.max(self.y) #since y starts from 0
-    self.classes_ = list(range(no_of_classifiers+1))
-    if isinstance(self.clf_args,list):
-      #for pipelines
-      c = self.classifier(self.clf_args)
-    elif isinstance(self.clf_args,dict):
-      #for normal estimators
-       c = self.classifier(**self.clf_args)
-    for i in range(no_of_classifiers):
-      # make a copy of y because we want to change the values of y
-      copy_y = np.copy(self.y)
-      # make a binary classification here
-      copy_y[copy_y<=i] = 0
-      copy_y[copy_y>i] = 1
-      classifier = copy.deepcopy(c)
-      classifier.fit(self.X,copy_y,**fit)
-      self.clfs.append(classifier)
-    return self
-  def predict_proba(self,test):
-    prob_list = []
-    final_prob = []
-    length = len(self.clfs)
-    for clf in self.clfs:
-      prob_list.append(clf.predict_proba(test)[:,1])
-    for i in range(length+1):
-      if i == 0:
-        final_prob.append(1-prob_list[i])
-      elif i == length:
-        final_prob.append(prob_list[i-1])
-      else:
-        final_prob.append(prob_list[i-1]-prob_list[i])
-    answer = np.array(final_prob).transpose()
-    self.final_prob= answer
-    return answer
-  def predict(self,test):
-    self.predict_proba(test)
-    return np.argmax(self.final_prob,axis=1)
-  def score(self,X,y,sample_weight=None):
-    from sklearn.metrics import accuracy_score
-    return accuracy_score(y, self.predict(X), sample_weight=sample_weight)
+
 # %%
 # convert y values to start from 0 (0 -> 4) and convert to int from float
-y_train = (y_train - 1).astype(int)
-y_test = (y_test - 1).astype(int)
+y_train_ord = (y_train - 1).astype(int)
+y_test_ord = (y_test - 1).astype(int)
 
 clf = OrdClass(SVC,clf_args={'kernel':'linear', 'probability':True})
-clf.fit(X_train,y_train)
+clf.fit(X_train, y_train_ord)
 y_pred_ord = clf.predict(X_test)
-print('Ordinal classification report:\n', classification_report(y_test, y_pred_ord))
+print('Ordinal classification report:\n', classification_report(y_test_ord, y_pred_ord))
 # %%
